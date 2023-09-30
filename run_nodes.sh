accf63fdcb5ec76a47e5b94c7f8ced1f6eb588c9
cwpetrich <conradpetrich@gmail.com>
2023-09-29 18:15:41 -0600

    fix: correct position of option flags for read

--- run_nodes.sh
+++ run_nodes.sh
@@ -3,7 +3,7 @@ if ! [ -f $file ]; then
     cp ~/node-runner/.env ~/node-runner/.env.local
     read -p "Node Name: " node_name
     read -p "Username: " node_username
-    read -sp "Password: " node_password
+    read -ps "Password: " node_password
     sed -i "s/<nodename>/${node_name}/" $file
     sed -i "s/<username>/${node_username}/" $file
     sed -i "s/<password>/${node_password}/" $file
