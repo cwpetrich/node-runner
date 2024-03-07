e213833a850559dc2ed9d94ad0add5a90f8eecfa
JAllred <josh.allred@outlook.com>
2024-03-07 10:26:48 -0700

    fix indentation

--- run_nodes.sh
+++ run_nodes.sh
@@ -6,13 +6,13 @@ BASEDIRECTORY=$(dirname "$SCRIPT")
 file=$BASEDIRECTORY/.env.local 
 
 if ! [ -f $file ]; then 
-cp $BASEDIRECTORY/.env $BASEDIRECTORY/.env.local 
-read -p "Node Name: " node_name 
-read -p "Username: " node_username 
-read -sp "Password: " node_password 
-sed -i "s/<nodename>/${node_name}/" $file 
-sed -i "s/<username>/${node_username}/" $file 
-sed -i "s/<password>/${node_password}/" $file 
+    cp $BASEDIRECTORY/.env $BASEDIRECTORY/.env.local 
+    read -p "Node Name: " node_name 
+    read -p "Username: " node_username 
+    read -sp "Password: " node_password 
+    sed -i "s/<nodename>/${node_name}/" $file 
+    sed -i "s/<username>/${node_username}/" $file 
+    sed -i "s/<password>/${node_password}/" $file 
 fi 
 
 cd $BASEDIRECTORY 
